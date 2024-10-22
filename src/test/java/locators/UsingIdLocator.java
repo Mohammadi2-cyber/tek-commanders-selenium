a3aacb0cf4a438d5e2deb66074c0bf030e6c980a
Abdullah MOHAMMADI <ab.mohammadi791@gmail.com>
2024-10-22 14:56:03 -0400

    locators

---
+++ src/test/java/locators/UsingIdLocator.java
@@ -0,0 +1,21 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsingIdLocator {
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        //Step 1. locate the element.
+        By signInLink = By.id("signinLink");
+        // Step 2. ask Selenium to find the element.
+        WebElement signInElement = driver.findElement(signInLink);
+        // perform the action
+        signInElement.click();
+        driver.quit();
+
+    }
+}
---
+++ src/test/java/tek_selenium_week_1/BrowserSettings.java
@@ -0,0 +1,14 @@
+package tek_selenium_week_1;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class BrowserSettings {
+    public static void main(String[] args) {
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        String getTitle = driver.getTitle();
+        System.out.println(getTitle);
+        driver.quit();
+    }
+}
