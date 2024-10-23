44163863b6992f0465c1a390a3e012846776bc5e
Abdullah MOHAMMADI <ab.mohammadi791@gmail.com>
2024-10-23 19:21:21 -0400

    recap

---
+++ src/test/java/locators/RecapActivity.java
@@ -0,0 +1,32 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RecapActivity {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+        By signInLocator = By.id("signinLink");
+        WebElement signInElement = driver.findElement(signInLocator);
+        signInElement.click();
+        Thread.sleep(2000);
+        By wrongUserName = By.id("email");
+        WebElement wrongUserElement = driver.findElement(wrongUserName);
+        wrongUserElement.sendKeys("Wrong email");
+        Thread.sleep(2000);
+        By wrongPassword = By.id("password");
+        WebElement wrongPassElement = driver.findElement(wrongPassword);
+        wrongPassElement.sendKeys("abdullah88@tekschool.come");
+        Thread.sleep(2000);
+        By login = By.id("loginBtn");
+        WebElement loginBtnElement = driver.findElement(login);
+        loginBtnElement.click();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
